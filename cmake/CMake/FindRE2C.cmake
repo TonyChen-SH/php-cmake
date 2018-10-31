5d3542edd63b0b5eb941a77524c3292626febd2d
Tony Chen <chendan316@gmail.com>
2018-11-01 00:31:36 +0800

    update

--- cmake/CMake/FindRE2C.cmake
+++ cmake/CMake/FindRE2C.cmake
@@ -18,33 +18,34 @@ set(RE2C_FOUND FALSE)
 find_program(RE2C_EXECUTABLE NAMES re2c DOC "path to re2c executable")
 mark_as_advanced(RE2C_EXECUTABLE)
 
-if(RE2C_EXECUTABLE)
-  set(RE2C_FOUND TRUE)
-  # check version
-  execute_process(COMMAND ${RE2C_EXECUTABLE} --vernum
-    RESULT_VARIABLE RE2C_version_result
-    OUTPUT_VARIABLE RE2C_version_output
-    ERROR_VARIABLE RE2C_version_error
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-#  message("RE2C_version_result:${RE2C_version_result} RE2C_version_output:${RE2C_version_output} RE2C_version_error:${RE2C_version_error}")
-  if(RE2C_version_result EQUAL 0)
-    message(SEND_ERROR "Command \"${RE2C_EXECUTABLE} --vernum\" failed with output:\n${RE2C_version_error}")
-  else(RE2C_version_result EQUAL )
-    set(RE2C_VERNUM ${RE2C_version_output})
-  endif(RE2C_version_result EQUAL 0)
+if (RE2C_EXECUTABLE)
+    set(RE2C_FOUND TRUE)
+    # check version
+    execute_process(COMMAND ${RE2C_EXECUTABLE} --vernum
+            RESULT_VARIABLE RE2C_version_result
+            OUTPUT_VARIABLE RE2C_version_output
+            ERROR_VARIABLE RE2C_version_error
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+    #  message("RE2C_version_result:${RE2C_version_result} RE2C_version_output:${RE2C_version_output} RE2C_version_error:${RE2C_version_error}")
+    if (RE2C_version_result EQUAL 0)
+        # @TODO 先注释，后面再看看怎么处理
+        #    message(SEND_ERROR "Command \"${RE2C_EXECUTABLE} --vernum\" failed with output:\n${RE2C_version_error}")
+    else (RE2C_version_result EQUAL)
+        set(RE2C_VERNUM ${RE2C_version_output})
+    endif (RE2C_version_result EQUAL 0)
 
-  #set call macro
-  macro(RE2C_TARGET Input Output Args)
-    set(RE2C_EXECUTABLE_opts ${Args})
-    add_custom_command(OUTPUT ${Output}
-      COMMMAND ${RE2C_EXECUTABLE} ${RE2C_EXECUTABLE_opts} -o${Output} ${Input}
-#      DEPENDS ${Input}
-      COMMENT "[RE2C] Building re2c scanner with ${RE2C_EXECUTABLE} ${RE2C_VERSION}"
-      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
-    message("${RE2C_EXECUTABLE} ${RE2C_EXECUTABLE_opts} -o${Output} ${Input}")
-  endmacro(RE2C_TARGET)
-endif(RE2C_EXECUTABLE)
+    #set call macro
+    macro(RE2C_TARGET Input Output Args)
+        set(RE2C_EXECUTABLE_opts ${Args})
+        add_custom_command(OUTPUT ${Output}
+                COMMMAND ${RE2C_EXECUTABLE} ${RE2C_EXECUTABLE_opts} -o${Output} ${Input}
+                #      DEPENDS ${Input}
+                COMMENT "[RE2C] Building re2c scanner with ${RE2C_EXECUTABLE} ${RE2C_VERSION}"
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+        message("${RE2C_EXECUTABLE} ${RE2C_EXECUTABLE_opts} -o${Output} ${Input}")
+    endmacro(RE2C_TARGET)
+endif (RE2C_EXECUTABLE)
 
-if(NOT RE2C_FOUND)
-  message(STATUS "re2c was not found.")
-endif(NOT RE2C_FOUND)
+if (NOT RE2C_FOUND)
+    message(STATUS "re2c was not found.")
+endif (NOT RE2C_FOUND)
